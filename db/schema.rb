9456b85f99899c05036bc348327a99b8c924a383
Nick Lindeberg <lindebergnick@gmail.com>
2018-12-12 17:02:14 -0700

    adds test for activied users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_222750) do
+ActiveRecord::Schema.define(version: 2018_12_12_235414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2018_12_11_222750) do
     t.string "provider"
     t.string "uid"
     t.string "username"
+    t.boolean "activated", default: false
     t.index ["email"], name: "index_users_on_email"
   end
 
